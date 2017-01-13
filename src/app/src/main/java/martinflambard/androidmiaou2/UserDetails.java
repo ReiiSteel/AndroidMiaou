664f303ee132918999c29e07f6d7cbcd368dc100
wilfi00 <0johan.martin0@gmail.com>
2017-01-13 18:14:27 +0100

    First commit

---
+++ src/app/src/main/java/martinflambard/androidmiaou2/UserDetails.java
@@ -0,0 +1,11 @@
+package martinflambard.androidmiaou2;
+
+/**
+ * Created by wilfi on 13/01/2017.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
